899a89264c07ac7c9fc6760a1648d70645229e79
Natália Molini Buffa <nati.buffa1907@gmail.com>
2024-08-15 16:45:37 -0300

    pilha estatica

--- pilha 1308/src/Main.java
+++ pilha 1308/src/Main.java
@@ -1,2 +1,31 @@
 public class Main {
+    public static void main(String[] args) {
+        StaticStacks s = new StaticStacks();
+        s.push(2);
+        s.push(3);
+        s.push(40);
+        s.push(43);
+        s.push(21);
+        s.push(1);
+
+        System.out.println("__________________________");
+        System.out.println("Valor removido: " + s.pop());
+        System.out.println("Valor removido: " + s.pop());
+
+        System.out.println("__________________________");
+        s.clear();
+
+        System.out.println("__________________________");
+        s.pop();
+
+        System.out.println("__________________________");
+        s.size();
+        s.push(1);
+        s.push(7);
+        s.size();
+        s.push(2);
+        s.push(4);
+        s.push(8);
+        s.size();
+    }
 }
--- pilha 1308/src/StaticStacks.java
+++ pilha 1308/src/StaticStacks.java
@@ -8,7 +8,7 @@ public class StaticStacks {
 
     //cria um construtor para definir o tamanho da pilha e o topo (inicio) da pilha
     public StaticStacks(){
-        this.pilha = new Object[10];
+        this.pilha = new Object[5];
         this.topPilha = -1;
     }
 
@@ -30,9 +30,28 @@ public class StaticStacks {
     public void clear() {
         Arrays.fill(pilha, null);
         topPilha = -1;
+        System.out.println("Sua pilha foi esvaziada!");
     }
 
+    public void push(Object valor) {
+        //verifica se o topo da pilha é menor que o tamanho da pilha
+        if (this.topPilha < this.pilha.length - 1) {
+            //se for maior que menor ele insere o valor na pilha e incrementa o valor do indice
+            this.pilha[++topPilha] = valor;
+            System.out.println("Valor adicionado a pilha: " + valor);
+        } else if (this.topPilha == this.pilha.length - 1) {
+            System.out.println("Acabou o espaço!");
+        }
 
+    }
+    public Object pop(){
+        if(isEmpty()){
+            System.out.println("Sua pilha está vazia!");
+            return null;
+        }
+        //retorna o valor do ultimo elemento da pilha e decrementa o topo
+        return this.pilha[this.topPilha--];
+    }
     //cria um metodo para verificar o tamanho da pilha, ou seja, quantos itens tem dentro da pilha
     public int size(){
         if (this.isEmpty()){
@@ -40,8 +59,11 @@ public class StaticStacks {
             return 0;
         } else if (this.isFull()) {
             System.out.println("Sua pilha está cheia!");
-            return 10;
+            return 5;
         }
+        System.out.print("Sua pilha possui ");
+        System.out.print(this.topPilha + 1);
+        System.out.println(" itens!");
         return this.topPilha + 1;
     }
 }
