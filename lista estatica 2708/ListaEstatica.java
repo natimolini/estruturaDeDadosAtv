950f94531510743f5e7699b2f6bd3bd9dc488e27
Natália Molini Buffa <nati.buffa1907@gmail.com>
2024-08-27 12:20:20 -0300

    .

---
+++ lista estatica 2708/ListaEstatica.java
@@ -0,0 +1,24 @@
+public class ListaEstatica {
+    private Object[] lista;
+    private int tamanho;
+    private int capacidade;
+
+    public ListaEstatica(int capacidade){
+        this.tamanho = 0;
+        this.lista = new Object[capacidade];
+        this.capacidade = capacidade;
+    }
+
+    public boolean isFull(){
+        return tamanho == capacidade;
+    }
+    public boolean isEmpty(){
+        return tamanho == 0;
+    }
+    public void add(int valor){
+        if (isFull()){
+            System.out.println("Sua lista está cheia!");
+        }
+
+    }
+}
