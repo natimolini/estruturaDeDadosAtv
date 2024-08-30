55954c4b65c05c915ef7756aff0a19daa3fed380
Natália Molini Buffa <nati.buffa1907@gmail.com>
2024-08-30 18:16:44 -0300

    .

--- lista estatica 2708/ListaEstatica.java
+++ lista estatica 2708/ListaEstatica.java
@@ -1,12 +1,12 @@
 public class ListaEstatica {
 
-    private Object[] lista;
+    private int[] lista;
     private int tamanho;
     private int capacidade;
 
     public ListaEstatica(int capacidade){
         this.tamanho = 0;
-        this.lista = new Object[capacidade];
+        this.lista = new int[capacidade];
         this.capacidade = capacidade;
     }
 
@@ -16,12 +16,50 @@ public class ListaEstatica {
     public boolean isEmpty(){
         return tamanho == 0;
     }
-    public void add(int valor){
+    public void addFim(int valor){
         if (isFull()){
             System.out.println("Sua lista está cheia!");
             return;
         }
         this.lista[tamanho-1]=valor;
     }
-    
+    public void add(int valor, int indice){
+        if (lista[indice] != 0){
+            ir(indice);
+        }
+        this.lista[indice] = valor;
+    }
+    public int tirar(int indice){
+        if (lista[indice] != 0){
+            int valor = lista[indice];
+            voltar(indice);
+            return valor;
+        }
+        return 0;
+    }
+    public void ir(int indice){
+        if (!isFull() && indice < tamanho-1 && indice>=0){
+            for (int i = tamanho; i > indice; i--){
+                this.lista[i] =i-1;
+            }
+        }
+    }
+    public void voltar(int indice){
+        if (!isFull() && indice < tamanho-1 && indice>=0){
+            for (int i = tamanho; i > indice; i++){
+                this.lista[i] =i+1;
+            }
+        }
+    }
+    public int encontrar(int valor) {
+        for (int i = 0; i < tamanho - 1; i++) {
+            if (valor == lista[i]) {
+                return i;
+            }
+        }
+        return 0;
+    }
+    public int getData(int indice){
+        return lista[indice];
+    }
 }
--- lista estatica 2708/Main.java
+++ lista estatica 2708/Main.java
@@ -1,2 +1,8 @@
 public class Main {
+    public static void main(String[] args) {
+        ListaEstatica l = new ListaEstatica(5);
+
+        System.out.println("---Lista Estática---");
+
+    }
 }
